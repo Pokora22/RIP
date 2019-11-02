a50dd32c1b58345192943daf42f57dbd0a20314d
Pokora22 <pokora222@gmail.com>
2019-11-02 12:52:31 +0000

    Added difficulty handler in options

---
+++ Assets/Scripts/DifficultyHandler_scr.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DifficultyHandler_scr : MonoBehaviour
+{
+    private int difficulty;
+    private string[] diffNames = new[] {"Easy", "Normal", "Hard"};
+    private TextMeshProUGUI TMPro;
+
+    private void Start()
+    {
+        TMPro = GetComponentInChildren<TextMeshProUGUI>();
+        difficulty = PlayerPrefs.GetInt("difficulty");
+        TMPro.text = diffNames[difficulty];
+    }
+
+    public void changeDiff()
+    {
+        difficulty = (difficulty + 1) % 3;
+        TMPro.text = diffNames[difficulty];
+        PlayerPrefs.SetInt("difficulty", difficulty);
+    }
+}
