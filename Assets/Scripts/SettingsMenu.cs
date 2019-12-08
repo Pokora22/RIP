7dfb27960fb766656e26d04b4d5f4f8060ccad99
Pokora22 <pokora222@gmail.com>
2019-12-08 20:14:34 +0000

    Updating unity (hope nothing breaks)

--- Assets/Scripts/SettingsMenu.cs
+++ Assets/Scripts/SettingsMenu.cs
@@ -1,12 +1,30 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class SettingsMenu : MonoBehaviour
 {
 
-    public void SetVolume(float volume)
+    public AudioMixer AudioMixer;
+
+    public void SetMasterVolume(float volume)
     {
-        Debug.Log(volume);
+        AudioMixer.SetFloat("masterVolume", volume);
+    }
+    
+    public void SetMusicVolume(float volume)
+    {
+        AudioMixer.SetFloat("musicVolume", volume);
+    }
+    
+    public void SetSFXVolume(float volume)
+    {
+        AudioMixer.SetFloat("sfxVolume", volume);
+    }
+
+    public void SetQuality(int quality)
+    {
+        QualitySettings.SetQualityLevel(quality);
     }
 }
